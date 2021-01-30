a35f09e0e7b09b22cf95cef098a52c632ef4ad2b
Asta DK <nghiepnguyenfb@gmail.com>
2021-01-30 21:36:18 +0700

    [IMPROVE] Docker file

--- config/main.config.ts
+++ config/main.config.ts
@@ -2,6 +2,4 @@ export const MAIN_CONFIG = {
   port: process.env.PORT || 3000,
   mongoURL: process.env.MONGO_DB_URL || "mongodb://mongo:27017/nivime",
   env: process.env.NODE_ENV || "development",
-  useNameDB: process.env.MONGO_INITDB_ROOT_USERNAME || "admin",
-  passwordDB: process.env.MONGO_INITDB_ROOT_PASSWORD || "admin",
 };
